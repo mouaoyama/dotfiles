0ad0f83d075db303fac44d4fea2f353c4084481e
m.aoyama <moudev731010@gmail.com>
2024-05-25 22:50:00 +0900

    fix: update defaults to add trackpad clicking

--- defaults.sh
+++ defaults.sh
@@ -59,6 +59,7 @@ defaults write -g com.apple.trackpad.scaling 3
 defaults write com.apple.AppleMultitouchTrackpad FirstClickThreshold -int 0
 
 # Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
