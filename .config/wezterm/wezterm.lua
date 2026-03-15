980cadd98117b670745729199ea9ce478d9fc89e
m.aoyama <121992306+mouaoyama@users.noreply.github.com>
2026-03-15 22:58:38 +0900

    feat: update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,30 +1,22 @@
 local wezterm = require 'wezterm'
-
 local config = {}
 if wezterm.config_builder then
     config = wezterm.config_builder()
 end
-
 local act = wezterm.action
--- cool
--- local selected_scheme = 'iceberg-dark'
--- dark
--- local selected_scheme = 'NvimDark'
--- local selected_scheme = 'Gruvbox Dark'
--- local selected_scheme = 'iTerm2 Default'
--- local selected_scheme = 'zenwritten_dark'
+
 local selected_scheme = 'Dracula (Official)';
 local scheme = wezterm.get_builtin_color_schemes()[selected_scheme]
-
-config.window_background_opacity = 0.95
-config.window_close_confirmation = 'NeverPrompt'
-
 config.color_scheme = selected_scheme
 config.colors = {
     tab_bar = {
         active_tab = { bg_color = scheme.background, fg_color = scheme.foreground },
     }
 }
+
+config.macos_window_background_blur = 10
+config.window_background_opacity = 0.95
+config.window_close_confirmation = 'NeverPrompt'
 
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }
 config.keys = {
@@ -48,11 +40,14 @@ config.keys = {
     { key = '%', mods = 'LEADER|SHIFT', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
     { key = "'", mods = 'LEADER|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
     { key = 'o', mods = 'LEADER', action = act.ActivatePaneDirection 'Next' },
+    { key = 'x', mods = 'LEADER', action = act.CloseCurrentPane { confirm = true } },
     { key = 'z', mods = 'LEADER', action = act.TogglePaneZoomState },
     { key = '{', mods = 'LEADER|SHIFT', action = act.RotatePanes 'CounterClockwise' },
     { key = '}', mods = 'LEADER|SHIFT', action = act.RotatePanes 'Clockwise' },
     { key = 'q', mods = 'LEADER', action = act.PaneSelect },
     -- vim: pane
+    { key = "-", mods = "LEADER", action = act.SplitVertical },
+    { key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal },
     { key = 'h', mods = 'LEADER', action = act.ActivatePaneDirection('Left') },
     { key = 'j', mods = 'LEADER', action = act.ActivatePaneDirection('Down') },
     { key = 'k', mods = 'LEADER', action = act.ActivatePaneDirection('Up') },
@@ -60,7 +55,9 @@ config.keys = {
     { key = 'H', mods = 'LEADER|SHIFT', action = act.AdjustPaneSize({ 'Left', 5 }) },
     { key = 'J', mods = 'LEADER|SHIFT', action = act.AdjustPaneSize({ 'Down', 5 }) },
     { key = 'K', mods = 'LEADER|SHIFT', action = act.AdjustPaneSize({ 'Up', 5 }) },
-    { key = 'L', mods = 'LEADER|SHIFT', action = act.AdjustPaneSize({ 'Right', 5 }) }
+    { key = 'L', mods = 'LEADER|SHIFT', action = act.AdjustPaneSize({ 'Right', 5 }) },
+    -- claude code with tmux
+    -- { key = 'Enter', mods = 'SHIFT', action = act.SendString('\x1b[13;2u') },
 }
 
 return config
